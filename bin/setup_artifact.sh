778387087684ca2c509fcb38ebbc0c2e5109f16f
Lin-Mao <linmaodev@gmail.com>
2025-12-09 21:16:26 +0800

    Remove external dependencies

--- bin/setup_artifact.sh
+++ bin/setup_artifact.sh
@@ -6,8 +6,6 @@ if [ $(basename $CURRENT_DIR) != "cgo26-ae" ]; then
     exit 1
 fi
 
-git clone https://github.com/AccelProf/benchmarks.git
-
 
 # Install dependencies
 pip install regex
@@ -15,11 +13,6 @@ pip install matplotlib
 pip install circlify
 pip install pandas
 pip install git+https://github.com/openai/whisper.git
-
-pip install gdown
-
-gdown https://drive.google.com/uc?id=1ETHKAvit9BurcypF8ZGm6fj4ORCdpxLg
-unzip pre-results.zip && rm pre-results.zip
 
 
 # Create directories
